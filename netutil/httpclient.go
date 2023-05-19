9497c89fdb624ea3b210500dbfc397f8d78a1846
WangZY <hnwzy@live.com>
2023-05-19 19:56:17 +0800

    go get -u

--- netutil/httpclient.go
+++ netutil/httpclient.go
@@ -7,10 +7,7 @@ import (
 	"context"
 	"encoding/json"
 	"io"
-	"net"
 	"net/http"
-	"net/url"
-	"strings"
 	"time"
 )
 
@@ -31,44 +28,58 @@ func NewClient(trip ...http.RoundTripper) HTTPClient {
 	}
 }
 
+func (c HTTPClient) Do(req *http.Request) (*http.Response, error) {
+	return c.cli.Do(req)
+}
+
 // Fetch 发送请求
-func (c HTTPClient) Fetch(ctx context.Context, method string, addr *url.URL, body io.Reader, header http.Header) (*http.Response, error) {
+func (c HTTPClient) Fetch(ctx context.Context, method string, addr string, body io.Reader, header http.Header) (*http.Response, error) {
 	return c.fetch(ctx, method, addr, body, header)
 }
 
-// Do 发送请求
-func (c HTTPClient) Do(ctx context.Context, method, addr string, body io.Reader, header http.Header) (*http.Response, error) {
-	u, err := url.Parse(addr)
-	if err != nil {
-		return nil, err
-	}
-	return c.fetch(ctx, method, u, body, header)
-}
-
-// DoJSON 发送 JSON 请求
-func (c HTTPClient) DoJSON(ctx context.Context, method, addr string, body, resp any, header http.Header) error {
+// JSON 发送 JSON 响应 JSON
+func (c HTTPClient) JSON(ctx context.Context, method, addr string, body, resp any, header http.Header) error {
 	res, err := c.doJSON(ctx, method, addr, body, header)
 	if err != nil {
 		return err
 	}
-
 	//goland:noinspection GoUnhandledErrorResult
 	defer res.Body.Close()
 
 	return json.NewDecoder(res.Body).Decode(resp)
 }
 
+// SilentJSON 发送 JSON 请求但不关注响应结果，只关注是否存在错误
+func (c HTTPClient) SilentJSON(ctx context.Context, method, addr string, req any, header http.Header) error {
+	res, err := c.doJSON(ctx, method, addr, req, header)
+	if err == nil {
+		_ = res.Body.Close()
+	}
+	return err
+}
+
 // DoJSONTimeout 超时控制发送请求
 func (c HTTPClient) DoJSONTimeout(timeout time.Duration, method, addr string, body, resp any, header http.Header) error {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	err := c.DoJSON(ctx, method, addr, body, resp, header)
+	err := c.JSON(ctx, method, addr, body, resp, header)
 	cancel()
 	return err
 }
 
 // NewRequest 构造 http.Request
-func (c HTTPClient) NewRequest(ctx context.Context, method string, addr *url.URL, body io.Reader, header http.Header) *http.Request {
-	return c.newRequest(ctx, method, addr, body, header)
+func (c HTTPClient) NewRequest(ctx context.Context, method string, addr string, body io.Reader, header http.Header) (*http.Request, error) {
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	req, err := http.NewRequestWithContext(ctx, method, addr, body)
+	if err != nil {
+		return nil, err
+	}
+	if len(header) != 0 {
+		req.Header = header
+	}
+
+	return req, nil
 }
 
 func (c HTTPClient) doJSON(ctx context.Context, method, addr string, body any, header http.Header) (*http.Response, error) {
@@ -82,25 +93,20 @@ func (c HTTPClient) doJSON(ctx context.Context, method, addr string, body any, h
 	header.Set("Accept", "application/json")
 	header.Set("Content-Type", "application/json; charset=utf-8")
 
-	return c.do(ctx, method, addr, rd, header)
-}
-
-func (c HTTPClient) do(ctx context.Context, method, addr string, body io.Reader, header http.Header) (*http.Response, error) {
-	u, err := url.Parse(addr)
-	if err != nil {
-		return nil, err
-	}
-	return c.fetch(ctx, method, u, body, header)
+	return c.fetch(ctx, method, addr, rd, header)
 }
 
 // fetch 发送请求
-func (c HTTPClient) fetch(ctx context.Context, method string, addr *url.URL, body io.Reader, header http.Header) (*http.Response, error) {
-	if addr == nil {
-		return nil, &net.AddrError{Err: "target url is nil"}
+func (c HTTPClient) fetch(ctx context.Context, method string, addr string, body io.Reader, header http.Header) (*http.Response, error) {
+	req, err := c.NewRequest(ctx, method, addr, body, header)
+	if err != nil {
+		return nil, err
 	}
 
-	req := c.NewRequest(ctx, method, addr, body, header)
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
+	encoding := "Accept-Encoding"
+	if req.Header.Get(encoding) == "" {
+		req.Header.Set("Accept-Encoding", "gzip, deflate")
+	}
 	res, err := c.cli.Do(req)
 	if err != nil {
 		return nil, err
@@ -136,90 +142,6 @@ func (c HTTPClient) fetch(ctx context.Context, method string, addr *url.URL, bod
 	}
 
 	return nil, err
-}
-
-// newRequest 构造 http.Request
-func (c HTTPClient) newRequest(ctx context.Context, method string, addr *url.URL, body io.Reader, header http.Header) *http.Request {
-	if method == "" {
-		method = http.MethodGet
-	}
-	if addr == nil {
-		addr = &url.URL{Scheme: "about", Opaque: "blank"} // about:blank
-	}
-	rc, ok := body.(io.ReadCloser)
-	if !ok && body != nil {
-		rc = io.NopCloser(body)
-	}
-
-	req := &http.Request{
-		Method:     method,
-		URL:        addr,
-		Proto:      "HTTP/1.1",
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header:     header,
-		Body:       rc,
-		Host:       addr.Host,
-	}
-	if req.Header == nil {
-		req.Header = make(http.Header, 2)
-	}
-	host := req.Header.Get("Host")
-	if host == "" {
-		host = addr.Host
-	}
-	if strings.LastIndex(host, ":") > strings.LastIndex(host, "]") {
-		host = strings.TrimSuffix(host, ":")
-	}
-	if body != nil {
-		switch v := body.(type) {
-		case *bytes.Buffer:
-			req.ContentLength = int64(v.Len())
-			buf := v.Bytes()
-			req.GetBody = func() (io.ReadCloser, error) {
-				r := bytes.NewReader(buf)
-				return io.NopCloser(r), nil
-			}
-		case *bytes.Reader:
-			req.ContentLength = int64(v.Len())
-			snapshot := *v
-			req.GetBody = func() (io.ReadCloser, error) {
-				r := snapshot
-				return io.NopCloser(&r), nil
-			}
-		case *strings.Reader:
-			req.ContentLength = int64(v.Len())
-			snapshot := *v
-			req.GetBody = func() (io.ReadCloser, error) {
-				r := snapshot
-				return io.NopCloser(&r), nil
-			}
-		default:
-			// This is where we'd set it to -1 (at least
-			// if body != NoBody) to mean unknown, but
-			// that broke people during the Go 1.8 testing
-			// period. People depend on it being 0 I
-			// guess. Maybe retry later. See Issue 18117.
-		}
-		// For client requests, Request.ContentLength of 0
-		// means either actually 0, or unknown. The only way
-		// to explicitly say that the ContentLength is zero is
-		// to set the Body to nil. But turns out too much code
-		// depends on NewRequest returning a non-nil Body,
-		// so we use a well-known ReadCloser variable instead
-		// and have the http package also treat that sentinel
-		// variable to mean explicitly zero.
-		if req.GetBody != nil && req.ContentLength == 0 {
-			req.Body = http.NoBody
-			req.GetBody = func() (io.ReadCloser, error) { return http.NoBody, nil }
-		}
-	}
-
-	if ctx == nil {
-		ctx = context.Background()
-	}
-
-	return req.WithContext(ctx)
 }
 
 // toJSON 转为 JSON
