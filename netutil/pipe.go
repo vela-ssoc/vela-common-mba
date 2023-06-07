7da38fca26ed2357880037138fcddfd6e3224452
WangZY <hnwzy@live.com>
2023-06-07 11:16:09 +0800

    1. 流式 IO 拷贝

--- netutil/pipe.go
+++ netutil/pipe.go
@@ -3,102 +3,91 @@ package netutil
 import (
 	"io"
 	"net"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
-func TwoSockPIPE(a, b *websocket.Conn) {
-	p := &twoSock{a: a, b: b}
-	p.exchange()
+func Pipe(a net.Conn, b *websocket.Conn) PipeStat {
+	bw := warpRWC(b)
+
+	var ret PipeStat
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		wn, err := io.CopyBuffer(bw, a, make([]byte, 4096))
+		_ = bw.Close()
+		ret.Atob = wn
+		ret.AErr = err
+	}()
+
+	wn, err := io.CopyBuffer(a, bw, make([]byte, 4096))
+	_ = a.Close()
+	ret.Btoa = wn
+	ret.BErr = err
+	wg.Wait()
+
+	return ret
 }
 
-type twoSock struct {
-	a, b *websocket.Conn
+type PipeStat struct {
+	Atob int64 // a 发送给 b 的字节数
+	Btoa int64 // b 发送给 a 的字节数
+	AErr error
+	BErr error
 }
 
-func (ts *twoSock) exchange() {
-	defer ts.close()
-	go ts.writeTo(ts.b, ts.a)
-	ts.writeTo(ts.a, ts.b)
+func PipeWebsocket(a, b *websocket.Conn) PipeStat {
+	aw := warpRWC(a)
+	bw := warpRWC(b)
+
+	var ret PipeStat
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		wn, err := io.CopyBuffer(bw, aw, make([]byte, 4096))
+		_ = bw.Close()
+		ret.Atob = wn
+		ret.AErr = err
+	}()
+
+	wn, err := io.CopyBuffer(aw, bw, make([]byte, 4096))
+	_ = aw.Close()
+	ret.Btoa = wn
+	ret.BErr = err
+	wg.Wait()
+
+	return ret
 }
 
-func (ts *twoSock) writeTo(dst, src *websocket.Conn) {
-	buf := make([]byte, 1024)
-	for {
-		mt, rd, err := src.NextReader()
-		if err != nil {
-			_ = dst.Close()
-			break
-		}
-		wt, err := dst.NextWriter(mt)
-		if err != nil {
-			_ = src.Close()
-			break
-		}
-		if _, err = io.CopyBuffer(wt, rd, buf); err != nil {
-			break
-		}
-		_ = wt.Close()
+func warpRWC(c *websocket.Conn) io.ReadWriteCloser {
+	return &websocketRWC{
+		c: c,
+		r: websocket.JoinMessages(c, ""),
 	}
 }
 
-func (ts *twoSock) close() {
-	_ = ts.a.Close()
-	_ = ts.b.Close()
+type websocketRWC struct {
+	c *websocket.Conn
+	r io.Reader
 }
 
-func ConnSockPIPE(c net.Conn, w *websocket.Conn) {
-	pp := &connPIPE{conn: c, sock: w}
-	pp.serve()
+func (w *websocketRWC) Read(p []byte) (int, error) {
+	return w.r.Read(p)
 }
 
-type connPIPE struct {
-	conn net.Conn
-	sock *websocket.Conn
-}
-
-func (pe *connPIPE) serve() {
-	defer pe.close()
-
-	go pe.connToSock()
-
-	buf := make([]byte, 1024)
-	for {
-		_, rd, err := pe.sock.NextReader()
-		if err != nil {
-			_ = pe.conn.Close()
-			break
-		}
-		n, err := rd.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				continue
-			}
-			break
-		}
-
-		if _, err = pe.conn.Write(buf[:n]); err != nil {
-			_ = pe.sock.Close()
-		}
+func (w *websocketRWC) Write(p []byte) (int, error) {
+	n := len(p)
+	var err error
+	if n != 0 {
+		err = w.c.WriteMessage(websocket.BinaryMessage, p)
 	}
+
+	return n, err
 }
 
-func (pe *connPIPE) connToSock() {
-	buf := make([]byte, 1024)
-	for {
-		n, err := pe.conn.Read(buf)
-		if err != nil {
-			_ = pe.sock.Close()
-			break
-		}
-		if err = pe.sock.WriteMessage(websocket.BinaryMessage, buf[:n]); err != nil {
-			_ = pe.conn.Close()
-			break
-		}
-	}
-}
-
-func (pe *connPIPE) close() {
-	_ = pe.conn.Close()
-	_ = pe.sock.Close()
-}
+func (w *websocketRWC) Close() error { return w.c.Close() }
