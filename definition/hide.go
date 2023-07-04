58c30cfb7bde3fb7401dd87604f7525799a6e401
WangZY <hnwzy@live.com>
2023-07-04 15:49:39 +0800

    隐写模块新增静默模式的选项

--- definition/hide.go
+++ definition/hide.go
@@ -10,10 +10,10 @@ type MinionHide struct {
 	LAN        []string  `json:"lan"`
 	VIP        []string  `json:"vip"`
 	Edition    string    `json:"edition"`
-	Hash       string    `json:"hash"` // 原始二进制的哈希（不含隐写内容）
-	Size       int64     `json:"size"` // 原始二进制文件大小（不含隐写内容）
-	Tags       []string  `json:"tags"` // 通过链接下载时的一些标记
-	Clam       bool      `json:"clam"` // 是否静默模式，仅对新注册的节点有效
+	Hash       string    `json:"hash"`   // 原始二进制的哈希（不含隐写内容）
+	Size       int64     `json:"size"`   // 原始二进制文件大小（不含隐写内容）
+	Tags       []string  `json:"tags"`   // 通过链接下载时的一些标记
+	Unload     bool      `json:"unload"` // 不加载任何配置，仅在节点注册时生效
 	DownloadAt time.Time `json:"download_at"`
 }
 
